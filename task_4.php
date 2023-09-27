9f0ddd60e13fbfe3577b2d14403536e29e7f925d
Abu Sayem Amir <sayem74@gmail.com>
2023-09-27 16:26:32 +0600

    corrected grade calculation

--- task_4.php
+++ task_4.php
@@ -1,34 +1,34 @@
 <?php
+$studentGrades = [
+    'student_1' => ['Math' => 84, 'English' => 93, 'Science' => 78],
+    'student_2' => ['Math' => 87, 'English' => 95, 'Science' => 91],
+    'student_3' => ['Math' => 61, 'English' => 82, 'Science' => 81]
+];
 
-/*Task 4: Multidimensional Array
-
-Create a multidimensional array called $studentGrades to store the grades of three students. Each student has grades for three subjects: Math, English, and Science. Write a PHP function which takes "$studentGrades" as an argument to calculate and print the average grade for each student.*/
-
-$studentGrades = array(
-    array('Math' => 84, 'English' => 93, 'Science' => 78),
-    array('Math' => 87, 'English' => 95, 'Science' => 91),
-    array('Math' => 61, 'English' => 82, 'Science' => 81)
-);
-
-function studentAverageGrade($arg)
+function studentAverageGrade($studentGrades)
 {
 
-    $averages = array();
+    $student_names = array_keys($studentGrades);
 
-    foreach ($arg as $student) {
-        $total = array_sum($student);
-        $count = count($student);
-        $average = $total / $count;
-        $averages[] = $average;
+    $average_grades = [];
+    foreach ($student_names as $student_name) {
+        $student_grades = $studentGrades[$student_name];
+        $average_grade = array_sum($student_grades) / count($student_grades);
+        $average_grades[$student_name] = $average_grade;
     }
 
-    return $averages;
+    return $average_grades;
 }
 
-$averageGrades = studentAverageGrade($studentGrades);
 
-foreach ($averageGrades as $key => $average) {
-    echo "Student " . ($key + 1) . ": $average, ";
+$average_grades = studentAverageGrade($studentGrades);
+
+foreach ($average_grades as $student_name => $average_grade) {
+    echo "{$student_name} average grade: {$average_grade}\n";
 }
 
-//Output: Student 1: 85, Student 2: 91, Student 3: 74.666666666667, 
+/*Output: 
+student_1 average grade: 85
+student_2 average grade: 91
+student_3 average grade: 74.666666666667
+*/
